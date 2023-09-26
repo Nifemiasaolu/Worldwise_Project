0f6cf1b28a8d84dd9d146ffafb4f83c885efd475
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-26 22:31:09 +0100

    Feat: Cities Context API

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from "react"
+
+const CitiesContext = createContext()
+
+function CitiesProvider() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const BASE_URL = "http://localhost:9000";
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CitiesContext
