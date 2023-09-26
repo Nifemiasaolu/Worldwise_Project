b8eda44aebc0c456526844c759ad66cb4e190597
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-26 22:29:30 +0100

    Feat: App component

--- src/App.jsx
+++ src/App.jsx
@@ -13,27 +13,7 @@ import Form from "./components/Form";
 import City from "./components/City";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const BASE_URL = "http://localhost:9000";
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
+  
   return (
     <BrowserRouter>
       <Routes>
