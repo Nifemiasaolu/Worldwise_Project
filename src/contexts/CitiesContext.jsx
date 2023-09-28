1ac7e77f4e2fe1835cf9b1ca76910472ba424280
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-28 19:56:46 +0100

    Context API

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -38,6 +38,25 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: "POST",
+        body: JSON.stringify(newCity),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log()(data);
+    } catch {
+      alert("There was an error loading data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div>
       <CitiesContext.Provider
@@ -46,6 +65,7 @@ function CitiesProvider({ children }) {
           isLoading,
           currentCity,
           getCity,
+          createCity,
         }}
       >
         {children}
